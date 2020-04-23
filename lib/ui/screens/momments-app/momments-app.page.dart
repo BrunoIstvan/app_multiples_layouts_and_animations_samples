7942c07ad532f2831d168c06fe975deb52317a25
Bruno Istvan Campos Monteiro <bruno.istvan@gmail.com>
2020-04-23 08:09:42 -0300

    incluídos os arquivos da tela momments app

---
+++ lib/ui/screens/momments-app/momments-app.page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MommentsAppPage extends StatefulWidget {
+  @override
+  _MommentsAppPageState createState() => _MommentsAppPageState();
+}
+
+// desenvolver https://github.com/lawreyios/FlutterMoments.git
+
+class _MommentsAppPageState extends State<MommentsAppPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/ui/screens/shopnbuy/shopnbuy.page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ShopAndBuyPage extends StatefulWidget {
+  @override
+  _ShopAndBuyPageState createState() => _ShopAndBuyPageState();
+}
+
+// desenvolver https://github.com/lawreyios/flutter-shopnbuy.git
+class _ShopAndBuyPageState extends State<ShopAndBuyPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
